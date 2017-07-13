aca8dbb0e531abc0f19b2397db21102f56e00e03
Antonio Mamani <antonio.mq@gmail.com>
2017-07-13 10:26:10 -0400

    Added basic error handler

---
+++ course-api/src/main/java/io/demo/springcourse/error/MyResourceNotFoundException.java
@@ -0,0 +1,20 @@
+package io.demo.springcourse.error;
+
+public final class MyResourceNotFoundException extends RuntimeException {
+
+    public MyResourceNotFoundException() {
+        super();
+    }
+
+    public MyResourceNotFoundException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyResourceNotFoundException(final String message) {
+        super(message);
+    }
+
+    public MyResourceNotFoundException(final Throwable cause) {
+        super(cause);
+    }
+}
---
+++ course-api/src/main/java/io/demo/springcourse/error/RestResponseExceptionHandler.java
@@ -0,0 +1,68 @@
+package io.demo.springcourse.error;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseExceptionHandler extends ResponseEntityExceptionHandler {
+
+    public RestResponseExceptionHandler() {
+        super();
+    }
+
+    // API
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ErrorMessage> somethingWentWrong(Exception ex){
+    	ErrorMessage exceptionResponse = 
+    			new ErrorMessage(ex.getMessage(),
+    			"Wath else do you want to add?");
+    	return new ResponseEntity<ErrorMessage>(exceptionResponse, 
+    			new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    		
+    }
+
+    // 400
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(final HttpMessageNotReadableException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        final String bodyOfResponse = "This should be application specific";
+        // ex.getCause() instanceof JsonMappingException, JsonParseException // for additional information later on
+        return handleExceptionInternal(ex, bodyOfResponse, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        final String bodyOfResponse = "This should be application specific";
+        return handleExceptionInternal(ex, bodyOfResponse, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    // 403
+
+    // 404
+
+    @ExceptionHandler(value = { MyResourceNotFoundException.class })
+    protected ResponseEntity<Object> handleNotFound(final RuntimeException ex, final WebRequest request) {
+        final String bodyOfResponse = "This should be application specific";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    // 409
+
+    // 412
+
+    // 500
+
+    @ExceptionHandler({ NullPointerException.class, IllegalArgumentException.class, IllegalStateException.class })
+    /*500*/public ResponseEntity<Object> handleInternal(final RuntimeException ex, final WebRequest request) {
+        logger.error("500 Status Code", ex);
+        final String bodyOfResponse = "This should be application specific";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+}
